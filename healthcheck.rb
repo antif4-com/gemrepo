3eb055c45dee34edf8e774f4c0579261c2534cc2
eian <eian@antif4.com>
2025-06-19 17:24:56 -0700

    re-add 404 to healthcheck
    Turns out, Rack::Cascade keys off of the presence of the 404, not the
    lack of a response to pass things on. So actually needed to response to
    404 if HealthCheck is the firs tin the cascade, so that geminabox has
    something to replace. This makes me think maybe HealthCheck should be
    last afterall...

--- healthcheck.rb
+++ healthcheck.rb
@@ -4,7 +4,7 @@ class HealthCheck
     if env['PATH_INFO'] == '/up'
       [200, {'Content-Type' => 'text/plain'}, ['We good here homedog.']]
     else
-      [500, {'Content-Type' => 'text/plain'}, ['Expected geminabox to 404 after this.']]
+      [404, {'Content-Type' => 'text/plain'}, ['Sorry, it ain\'t here.']]
     end
   end
 end
